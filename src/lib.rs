f8df7c490e0da53c11f191f90967faca461a40c6
pesju <pesju13@gmail.com>
2024-02-22 18:33:55 +0800

    add 'catch'

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,122 @@
 #[macro_export]
 /// ## Ternary Operator
-/// 
+///
 /// condition ? true : false
-/// 
-/// ## Examples 
+///
+/// ## Examples
 /// ```rust
 /// use op::ternary;
-/// 
-/// let n = ternary!(true => 0; 1);
+///
+/// let n = ternary!(true => 0, 1);
 /// assert_eq!(n, 0);
-/// let n = ternary!(false => 0; 1);
+/// let n = ternary!(false => 0, 1);
 /// assert_eq!(n, 1);
 /// ````
 macro_rules! ternary {
     ($condition:expr => $true:expr; $false:expr) => {
-        if $condition { $true } else { $false }
+        if $condition {
+            $true
+        } else {
+            $false
+        }
+    };
+    ($condition:expr => $true:expr, $false:expr) => {
+        if $condition {
+            $true
+        } else {
+            $false
+        }
     };
 }
 #[macro_export]
 /// A simple macro that handle `Option<T>`.
 macro_rules! some {
+    ($arg:expr; $err:expr) => {
+        if let Some(v) = $arg {
+            v
+        } else {
+            $err
+        }
+    };
     ($arg:expr; ret) => {
-        if let Some(v) = $arg { v } else { return; }
+        if let Some(v) = $arg {
+            v
+        } else {
+            return;
+        }
     };
     ($arg:expr; ret $result:expr) => {
-        if let Some(v) = $arg { v } else { return $result; }
+        if let Some(v) = $arg {
+            v
+        } else {
+            return $result;
+        }
     };
 
     ($arg:expr; con) => {
-        if let Some(v) = $arg { v } else { continue; }
-    };
-    ($arg:expr; break) => {
-        if let Some(v) = $arg { v } else { break; }
-    };
-    ($arg:expr; break $end:expr) => {
-        if let Some(v) = $arg { v } else { break $end; }
+        if let Some(v) = $arg {
+            v
+        } else {
+            continue;
+        }
     };
 }
 #[macro_export]
 /// A simple macro that handle `Result<T, E>`.
 macro_rules! result {
+    ($arg:expr; $err:expr) => {
+        if let Ok(v) = $arg {
+            v
+        } else {
+            $err
+        }
+    };
     ($arg:expr; ret) => {
-        if let Ok(v) = $arg { v } else { return; }
+        if let Ok(v) = $arg {
+            v
+        } else {
+            return;
+        }
     };
     ($arg:expr; ret $result:expr) => {
-        if let Ok(v) = $arg { v } else { return $result; }
+        if let Ok(v) = $arg {
+            v
+        } else {
+            return $result;
+        }
     };
 
     ($arg:expr; con) => {
-        if let Ok(v) = $arg { v } else { continue; }
+        if let Ok(v) = $arg {
+            v
+        } else {
+            continue;
+        }
     };
-    ($arg:expr; break) => {
-        if let Ok(v) = $arg { v } else { break; }
-    };
-    ($arg:expr; break $end:expr) => {
-        if let Ok(v) = $arg { v } else { break $end; }
-    };
+}
+
+/// A simple macro that catch `Result<T, E>` and `Option<T>`
+///
+/// # Examples
+///  ```
+/// use op::catch;
+///
+/// let result1: Result<i32, i32> = Ok(1);
+/// let result2: Result<i32, i32> = Ok(2);
+/// assert_eq!(Ok(3), catch!(i32 -> Ok(result1? + result2?)));
+///
+/// let option1: Option<i32> = Some(1);
+/// let option2: Option<i32> = Some(2);
+/// assert_eq!(Some(3), catch!(Some(option1? + option2?)));
+/// ```
+#[macro_export]
+macro_rules! catch {
+    ($e:ident -> $stmt:stmt) => {{
+        let catch = || -> std::result::Result<_, $e> { $stmt };
+        catch()
+    }};
+    ($stmt:stmt) => {{
+        let catch = || -> std::option::Option<_> { $stmt };
+        catch()
+    }};
 }
